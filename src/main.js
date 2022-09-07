e034f38a9b972a4dce677604528b9e3f95d85c11
Marcos Atencia Martinez <Marcos>
2022-09-07 07:00:14 -0500

    igual que antes con el login bien

--- src/main.js
+++ src/main.js
@@ -6,19 +6,10 @@ import vuetify from './plugins/vuetify'
 
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
-const app = express()
-app.use(express.json())
-app.get('/', function(req, res){
-res.send('hhola mundo hp')
-})
+
+
 new Vue({
   vuetify,
   render: h => h(App)
   
 }).$mount('#app');
-
-const server = http.createServer(process.env.PORT || 3000);
-
-app.listen(server, function() {
-  console.log("servidor escuchando el puerto")
-})
